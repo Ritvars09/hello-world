0fee24ed9367e4de39d00caa4fe149caa7e7e663
Ritvars <richissh123@gmail.com>
2021-10-27 13:59:24 +0300

    commit

---
+++ src/main/java/com/example/ProjektsC/ProjektsCApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjektsC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektsCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektsCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjektsC/ProjektsCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjektsC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektsCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
